5f00ac2e1fa4563174a01736b6e5b1dde58c2c1e
rhommardev <rjmlp02@gmail.com>
2025-07-24 01:33:23 -0300

    textos novos nas clases

--- Program.cs
+++ Program.cs
@@ -6,15 +6,15 @@ Iphone iphone = new Iphone("123456789", "iPhone 12", "ED23456789012345", 128);
 Nokia nokia = new Nokia("987654321", "Nokia 3310", "123456789012345", 32);
 
 // Testando os métodos do iPhone
-Console.WriteLine("Testando iPhone:");
+Console.WriteLine("Testando celular iPhone:");
 iphone.Ligar();
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("WhatsApp");
 
 // Testando os métodos do Nokia
-Console.WriteLine("Testando Nokia:");
+Console.WriteLine("Testando celular Nokia:");
 nokia.Ligar();
 nokia.ReceberLigacao();
-nokia.InstalarAplicativo("Snake");
+nokia.InstalarAplicativo("Binance");
 
 Console.WriteLine("Teste realizado com sucesso!");
