61d45fe8a94615f3ce2278c14bf2d49221215240
rhommardev <rjmlp02@gmail.com>
2025-07-24 00:16:52 -0300

    indentificando  codigos

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,25 +2,26 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
+//propriedades
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
+//        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+//construtor
         public Smartphone(string numero)
         {
             Numero = numero;
             // TODO: Passar os parâmetros do construtor para as propriedades
         }
-
+//metodo para ligar 
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
         }
-
+//metodo para receber ligação
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
         }
-
+//metodo para instalar aplicativo
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
