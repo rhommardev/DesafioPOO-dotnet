f850648495f906091c4887327a6649482d6c976e
rhommardev <rjmlp02@gmail.com>
2025-07-24 01:21:02 -0300

    CRIADAS INSTACIAS NAS CLASES NOKIA E IPHONE

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -3,6 +3,16 @@ namespace DesafioPOO.Models
     // TODO: Herdar da classe "Smartphone"
     public class Nokia : Smartphone
     {
+        // TODO: Implementar o construtor
+        public Nokia(string numero, string modelo, string imei, int memoria)
+        : base(numero, modelo, imei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia.");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Iphone iphone = new Iphone("123456789", "iPhone 12", "ED23456789012345", 128);
+Nokia nokia = new Nokia("987654321", "Nokia 3310", "123456789012345", 32);
+
+// Testando os métodos do iPhone
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("WhatsApp");
+
+// Testando os métodos do Nokia
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Snake");
