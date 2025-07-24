433cdb92ee235a45d7ed465f04f212f6b09a32ba
rhommardev <rjmlp02@gmail.com>
2025-07-24 01:26:29 -0300

    implentacionmenssagens no console e realizado teste com sucesso

--- Program.cs
+++ Program.cs
@@ -6,11 +6,15 @@ Iphone iphone = new Iphone("123456789", "iPhone 12", "ED23456789012345", 128);
 Nokia nokia = new Nokia("987654321", "Nokia 3310", "123456789012345", 32);
 
 // Testando os métodos do iPhone
+Console.WriteLine("Testando iPhone:");
 iphone.Ligar();
 iphone.ReceberLigacao();
 iphone.InstalarAplicativo("WhatsApp");
 
 // Testando os métodos do Nokia
+Console.WriteLine("Testando Nokia:");
 nokia.Ligar();
 nokia.ReceberLigacao();
 nokia.InstalarAplicativo("Snake");
+
+Console.WriteLine("Teste realizado com sucesso!");
